c262e0052310887eaa8d7e277276e8e640b9acfd
Estêfenis Freitas <lopefl89@gmail.com>
2021-12-18 10:47:16 -0300

    update develop

--- src/api.js
+++ src/api.js
@@ -14,32 +14,4 @@ export default {
         return res.data.user;
       }),
   },
-  thread: {
-    createThread: (details) =>
-      axios
-        .post("backend/newthread", { details })
-        .then((res) => res.data.thread),
-    editThread: (details) =>
-      axios
-        .put("/backend/editthread", { details })
-        .then((res) => res.data.thread),
-    deleteThread: (threadId) =>
-      axios
-        .delete(`/backend/deletethread/${threadId}`)
-        .then((res) => res.data.message),
-  },
-  comment: {
-    createComment: (details) =>
-      axios
-        .post("/backend/newcomment", { details })
-        .then((res) => res.data.comment),
-    editComment: (details) =>
-      axios
-        .put("/backend/editcomment", { details })
-        .then((res) => res.data.comment),
-    deleteComment: (commentId) =>
-      axios
-        .delete(`/backend/deletecomment/${commentId}`)
-        .then((res) => res.data.message),
-  },
 };
--- src/rootReducer.jsx
+++ src/rootReducer.jsx
@@ -1,10 +1,6 @@
 import {combineReducers} from 'redux';
 import user from './reducers/user';
-import thread from './reducers/thread'
-import comment from'./reducers/comment'
 
 export default combineReducers({
-    user,
-    thread,
-    comment
+    user
 })
