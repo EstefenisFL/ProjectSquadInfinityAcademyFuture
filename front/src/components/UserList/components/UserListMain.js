a8cbf0d210863a20ade32f241421a7f747d3380f
Estêfenis Freitas <lopefl89@gmail.com>
2022-01-25 22:17:04 -0300

    add delete method

--- front/src/components/UserList/components/UserListMain.js
+++ front/src/components/UserList/components/UserListMain.js
@@ -7,19 +7,39 @@ class UserListMain extends React.Component{
     constructor(props){
         super(props);    
         this.state= {
+            value:'',
+            valueToUse:0,
             persons: [],
-        },
-        this.getUser=this.getUser.bind(this);
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.getUser = this.getUser.bind(this);
+        this.deleteUser = this.deleteUser.bind(this);
+        this.updateUser = this.updateUser.bind(this);
     }
+
+        handleChange(event){
+            this.setState({value: event.target.value});
+            this.valueToUse = 0;
+            if(this.valueToUse !== parseInt(event.target.value)){
+                this.valueToUse = parseInt(event.target.value);
+            }else{
+                this.valueToUse = 0;
+            }  
+        }
+
+        handleSubmit(event) {
+            alert('O usuário foi deletado com sucesso');
+            event.preventDefault();
+        }
         
         async getUser() {
             const response = await api.actions.getAllUsers(); 
             this.setState({ persons: response});
             console.log(response.data);
         }
-        async deleteUser(id) {
-            id = 6;
-            await api.actions.deleteUser(id);
+        async deleteUser() {
+            await api.actions.deleteUser(this.valueToUse);
         }
         async updateUser(id, data) {
             const response = await api.actions.updateUser(id, data); 
@@ -44,16 +64,22 @@ class UserListMain extends React.Component{
                 <h1>List of Persons</h1> 
                 {console.log(persons)}
                 {persons.map(person =>(
+                    <form onSubmit={this.handleSubmit}>
                     <li key={person.id_user}>     
                         <h2>
-                            <strong>Nome: </strong>
+                        <strong>
+                            <input type="checkbox" id="scales" name="scales" value={person.id_user} onChange={this.handleChange}/>
+                                Nome: </strong>
                             {person.user_nome}
                         </h2>
                         <p>
                             {person.user_endereco}
                         </p>
                     </li>
+                    </form>
+                    
                 ))}
+                
                 <Button onClick={this.getUser} primary >Listar</Button>
                 <Button onClick={this.updateUser} primary >Editar</Button>
                 <Button onClick={this.deleteUser} color="red" >Deletar</Button>
